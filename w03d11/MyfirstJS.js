32bbc46684dba23aefeb1b812d2d98be4f4dcfdf
alzahranihind <alzahrani.hind.offical@gmail.com>
2021-04-04 22:50:33 +0300

    first commit

---
+++ w03d11/MyfirstJS.js
@@ -0,0 +1,8 @@
+function myFirstFunction() {
+    document.getElementById("text").innerHTML = "Hello World";
+    document.write("Helllo");
+    document.write(45+98);
+    window.alert("This is an alert box");
+    console.log("Hi there!!!");
+  }
+  
---
+++ w03d12/functions.js
@@ -0,0 +1,11 @@
+function myFunction (a, b) {
+    return a+b;
+}
+
+function myFunction2 (a, b) {
+    console.log(a+b) ;
+}
+
+function myFunction3 (a, b) {
+   document.write("a+b");
+}
