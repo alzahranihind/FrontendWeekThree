41cd6ef223fa58bffa8615bbfeb7b74a4011e3aa
alzahranihind <alzahrani.hind.offical@gmail.com>
2021-04-06 01:17:26 +0300

    W03D13

---
+++ w03d13/arrays.js
@@ -0,0 +1,16 @@
+// to declare(define) an array
+var myArray = [23,56,778,89,0]; //not empty
+var myArray2 =[] //empty
+// to print all the array's element 
+console.log(myArray);
+// to print one of the array's element 
+console.log(myArray[0]); // first element
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+console.log(myArray[4]); // last element 
+console.log(myArray.length); // print the length or the number of element in an array 
+document.getElementById("demo").innerHTML = myArray[0]; // to put the value in index 0 in the html
+myArray[5]=66; // add 66 to the end of the array
+//array methods 
+var fruits=["Banana", "Orange", "Lemon", "Apple", "Mango"]; //array of strings 
